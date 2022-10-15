48413fed5ea0d5488603b681b31dc0c7baefd960
APrajapat10 <coolprajapat@gmail.com>
2022-10-15 15:16:55 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Routes>
+          <Route path="/" exact element={<ExercisesList />} />
+          <Route path="/edit/:id" element={<EditExercise />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
