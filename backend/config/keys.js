0f3210a529861ff0abd4e58b047e4070cd3f7134
APrajapat10 <coolprajapat@gmail.com>
2022-10-16 07:23:54 +0530

    Backend Working

---
+++ backend/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    // "mongodb+srv://APrajapat10:Anu42686*@cluster0.epz6mhh.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://APrajapat10:Anu42686*@cluster2.aub4hee.mongodb.net/test",
+  secretOrKey: "secret",
+};
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,41 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
+const users = require("./routes/api/users");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
-
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri);
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
 
-const exercisesRouter = require("./routes/exercises");
-const usersRouter = require("./routes/users");
+// Passport middleware
+app.use(passport.initialize());
 
-app.use("/exercises", exercisesRouter);
-app.use("/users", usersRouter);
+// Passport config
+require("./config/passport")(passport);
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
